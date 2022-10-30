2b9b7c61924fc4cf83565084a5428ff5b3125d0f
apael <mikkomyllari@gmail.com>
2022-10-30 23:57:51 +0200

    removed unneccessary return

--- src/common/fightersView/FightersView.js
+++ src/common/fightersView/FightersView.js
@@ -35,10 +35,9 @@ const FightersView = props => {
       <div className="basis-5/6 h-full flex-col flex">
         <div className="bg-white flex gap-2 p-2 font-semibold">Omat taistelijat</div>
         <div className="flex flex-row flex-wrap overflow-auto justify-start">
-          {fighters?.map(fighter => {
-            return <FighterCard fighter={fighter} buttonText="Syö taistelija" key={fighter.id} onClick={eatFighter} />;
-            return;
-          })}
+          {fighters?.map(fighter => (
+            <FighterCard fighter={fighter} buttonText="Syö taistelija" key={fighter.id} onClick={eatFighter} />
+          ))}
         </div>
       </div>
     </React.Fragment>
