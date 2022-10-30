8f24f2da86f06b4621824111b2a6e362de1e22e8
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2022-10-30 18:56:12 +0200

    Added default user creation and storing

--- src/common/index.js
+++ src/common/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import BattleView from './battleView/BattleView';
 import ShopView from './shopView/ShopView';
 import FightersView from './fightersView/FightersView';
+import axios from 'axios';
+import { serverBaseUrl } from './Constants';
 
 const Home = () => {
+  const [user, setUser] = useState(localStorage.getItem('user'));
+
+  useEffect(() => {
+    if (!user || user === 'undefined') {
+      console.log('no user');
+      axios.post(`${serverBaseUrl}/start`, { name: 'dev' }).then((res) => {
+        setUser(res.data);
+        console.log(res.data);
+      });
+    }
+    localStorage.setItem('user', JSON.stringify(user));
+    console.log(user);
+  }, [user]);
+
   const navigate = useNavigate();
-  const user = { id: 0, money: 666 };
   return (
     <React.Fragment>
       <div className="bg-slate-600 h-screen flex flex-row">
