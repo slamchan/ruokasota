3c74b164d15ecae7b1d74165324ede85774205e2
Leo Qvarnström <leo.w.qvarnstrom@gmail.com>
2022-10-30 21:37:02 +0200

    Saved headaches

--- src/common/battleView/BattleView.js
+++ src/common/battleView/BattleView.js
@@ -35,82 +35,89 @@ const BattleView = (props) => {
 
   const findFirstDeath = () => {
     const deaths = battleResult?.combatLog.filter((x) => x.hpLeft <= 0);
-    return deaths.sort((a, b) => a - b)[0];
+    return deaths?.sort((a, b) => a - b)[0];
   };
 
   return (
     <React.Fragment>
       <div className="h-full basis-4/6">
-        <Modal
-          open={resultsOpen}
-          onClose={() => {
-            setResultsOpen(false);
-          }}
-          className="flex justify-center items-center"
-        >
-          <div className="bg-white h-2/3 w-2/3 p-2 overflow-auto font-semibold flex flex-col">
-            <div className="font-bold text-center border-b-black border-b-2">
-              {`${findFirstDeath().attacker} voitti taistelun!`}
-            </div>
-            <div className="flex flex-row">
-              <div>
-                {battleResult?.combatLog
-                  .sort((a, b) => a.timeStamp - b.timeStamp)
-                  .map((c) => {
-                    if (
-                      c.timeStamp <= findFirstDeath().timeStamp &&
-                      c.attacker === fighter.name
-                    ) {
-                      return (
-                        <div className=" p-2">
-                          {`Hyökkääjä: ${
-                            c.attacker
-                          } -  Vahinko: ${c.damage.toFixed(2)} - Puolustajan (${
-                            battleResult?.combatLog.find(
-                              (x) => x.attacker !== c.attacker
-                            ).attacker
-                          }) elämää jäljellä: ${c.hpLeft.toFixed(
-                            2
-                          )} - Aika taistelun alusta: ${c.timeStamp.toFixed(
-                            2
-                          )}s`}
-                        </div>
-                      );
-                    }
-                    return;
-                  })}
+        {battleResult && fighter && enemyFighter && (
+          <Modal
+            open={resultsOpen}
+            onClose={() => {
+              setResultsOpen(false);
+            }}
+            className="flex justify-center items-center"
+          >
+            <div className="bg-white h-2/3 w-2/3 p-2 overflow-auto font-semibold flex flex-col">
+              <div className="font-bold text-center border-b-black border-b-2">
+                {`${findFirstDeath().attacker} voitti taistelun!`}
               </div>
-              <div>
-                {battleResult?.combatLog
-                  .sort((a, b) => a.timeStamp - b.timeStamp)
-                  .map((c) => {
-                    if (
-                      c.timeStamp <=
-                        findFirstDeath(battleResult?.combatLog).timeStamp &&
-                      c.attacker === enemyFighter.name
-                    ) {
-                      return (
-                        <div className="p-2">
-                          {`Hyökkääjä: ${
-                            c.attacker
-                          } -  Vahinko: ${c.damage.toFixed(2)} - Puolustajan (${
-                            battleResult?.combatLog.find(
-                              (x) => x.attacker !== c.attacker
-                            ).attacker
-                          }) elämää jäljellä: ${c.hpLeft.toFixed(
-                            2
-                          )} - Aika taistelun alusta: ${c.timeStamp.toFixed(
-                            2
-                          )}s`}
-                        </div>
-                      );
-                    }
-                    return;
-                  })}
+              <div className="flex flex-row">
+                <div>
+                  {battleResult?.combatLog
+                    ?.sort((a, b) => a.timeStamp - b.timeStamp)
+                    .map((c) => {
+                      if (
+                        c.timeStamp <= findFirstDeath().timeStamp &&
+                        c.attacker === fighter.name
+                      ) {
+                        return (
+                          <div className=" p-2">
+                            {`Hyökkääjä: ${
+                              c.attacker
+                            } -  Vahinko: ${c.damage.toFixed(
+                              2
+                            )} - Puolustajan (${
+                              battleResult?.combatLog.find(
+                                (x) => x.attacker !== c.attacker
+                              ).attacker
+                            }) elämää jäljellä: ${c.hpLeft.toFixed(
+                              2
+                            )} - Aika taistelun alusta: ${c.timeStamp.toFixed(
+                              2
+                            )}s`}
+                          </div>
+                        );
+                      }
+                      return;
+                    })}
+                </div>
+                <div>
+                  {battleResult?.combatLog
+                    ?.sort((a, b) => a.timeStamp - b.timeStamp)
+                    .map((c) => {
+                      if (
+                        c.timeStamp <=
+                          findFirstDeath(battleResult?.combatLog).timeStamp &&
+                        c.attacker === enemyFighter.name
+                      ) {
+                        return (
+                          <div className="p-2">
+                            {`Hyökkääjä: ${
+                              c.attacker
+                            } -  Vahinko: ${c.damage.toFixed(
+                              2
+                            )} - Puolustajan (${
+                              battleResult?.combatLog.find(
+                                (x) => x.attacker !== c.attacker
+                              ).attacker
+                            }) elämää jäljellä: ${c.hpLeft.toFixed(
+                              2
+                            )} - Aika taistelun alusta: ${c.timeStamp.toFixed(
+                              2
+                            )}s`}
+                          </div>
+                        );
+                      }
+                      return;
+                    })}
+                </div>
               </div>
             </div>
-          </div>
-        </Modal>
+          </Modal>
+        )}
+
         <div className=" flex-row h-fit flex">
           <FighterCard
             fighter={testFighter}
